46e4dd1a8da7453dfed033c5ab20891a750ba619
Cody Brantley <cabarama@gmail.com>
2023-04-13 01:23:28 -0400

    refactored reservation form to formik
    added formik and yup functionality

--- src/components/ReservationForm.js
+++ src/components/ReservationForm.js
@@ -1,37 +1,47 @@
-import React, { useReducer, useRef } from "react";
+import React, { useEffect, useReducer } from "react";
 import { useNavigate } from 'react-router-dom';
 import { fetchAPI, submitAPI } from '../Api';
 import './ReservationForm.css';
+import { useFormik } from "formik";
+import * as Yup from 'yup';
 
 function ReservationForm({ onSubmit }) {
     const navigate = useNavigate();
     const todaysDate = new Date().toJSON().slice(0, 10);
-    const dateRef = useRef();
-    const timeRef = useRef();
-    const guestsRef = useRef();
-    const occasionRef = useRef();
-    const tablePreferencesRef = useRef();
-    const nameRef = useRef();
-    const emailAddressRef = useRef();
 
-    const handleSubmit = (e) => {
-        e.preventDefault();
-        const formData = {
-            date: dateRef.current.value,
-            time: timeRef.current.value,
-            guests: guestsRef.current.value,
-            occasion: occasionRef.current.value,
-            tablePreferences: tablePreferencesRef.current.value,
-            name: nameRef.current.value,
-            emailAddress: emailAddressRef.current.value,
-        };
+    const ReservationSchema = Yup.object({
+        date: Yup.date().required("Required").min(todaysDate),
+        name: Yup.string().required("Required"),
+        guests: Yup.number().min(1, "We need at least 1 guest").max(10, "Too many people"),
+        emailAddress: Yup.string().required("Required").email("Invalid email address"),
+        comment: Yup.string().required("Required").min(25, "Must be at least 25 characters")
+    });
 
-        if (submitAPI(formData)) {
-            if (onSubmit) onSubmit();
-            navigate('/reservations/thanks', { state: formData });
-        } else
-            return false;
-    }
+    const {
+        values,
+        errors,
+        touched,
+        getFieldProps,
+        handleSubmit,
+    } = useFormik({
+        initialValues: {
+            date: todaysDate,
+            time: '',
+            guests: 1,
+            occasion: 'Night Out',
+            tablePreferences: '',
+            name: '',
+            emailAddress: '',
+        },
+        validationSchema: ReservationSchema,
+        onSubmit: (formData) => {
+            if (submitAPI(formData)) {
+                if (onSubmit) onSubmit();
+                navigate('/reservations/thanks', { state: formData });
+            } else
+                return false;
+        },
+    });
 
     const updateTimes = (state, date) => {
         date = new Date(date);
@@ -44,28 +54,35 @@ function ReservationForm({ onSubmit }) {
 
     const [availableTimes, dispatch] = useReducer(updateTimes, {});
 
+    useEffect(() => {
+        dispatch(values.date);
+    }, [values.date]);
+
     return (
         <div className="container">
             <form id="form" className="reservation__form" onSubmit={handleSubmit}>
                 <div className="reservation__input_container">
                     <label className="reservation__label" htmlFor="date">Choose date</label>
-                    <input className="reservation__input" id="date" required type="date" name="date" min={todaysDate} ref={dateRef} onChange={(e) => dispatch(e.target.value)} />
+                    <input className="reservation__input" id="date" type="date" min={todaysDate} {...getFieldProps("date")} />
+                    {errors.date && touched.date && <div className="reservation__errors">{errors.date}</div>}
                 </div>
                 <div className="reservation__input_container">
                     <label className="reservation__label" htmlFor="time">Choose time</label>
-                    <select className="reservation__input" id="time" required name="time" ref={timeRef}>
-                        { (availableTimes && availableTimes.length >= 1) ? availableTimes.map(time => (
+                    <select className="reservation__input" id="time" {...getFieldProps("time")}>
+                        {(availableTimes && availableTimes.length >= 1) ? availableTimes.map(time => (
                             <option key={time}>{time}</option>
-                        )) : <option value="">--:--</option> }
+                        )) : <option value="">--:--</option>}
                     </select>
+                    {errors.time && touched.time && <div className="reservation__errors">{errors.time}</div>}
                 </div>
                 <div className="reservation__input_container">
                     <label className="reservation__label" htmlFor="guests">Number of guests</label>
-                    <input className="reservation__input" type="number" placeholder="1" min="1" max="10" id="guests" name="guests" ref={guestsRef} />
+                    <input className="reservation__input" type="number" placeholder="1" min="1" max="10" id="guests" {...getFieldProps("guests")} />
+                    {errors.guests && touched.guests && <div className="reservation__errors">{errors.guests}</div>}
                 </div>
                 <div className="reservation__input_container">
                     <label className="reservation__label" htmlFor="occasion">Occasion</label>
-                    <select className="reservation__input" id="occasion" name="occasion" ref={occasionRef}>
+                    <select className="reservation__input" id="occasion" {...getFieldProps("occasion")}>
                         <option>Night Out</option>
                         <option>Birthday</option>
                         <option>Anniversary</option>
@@ -73,15 +90,17 @@ function ReservationForm({ onSubmit }) {
                 </div>
                 <div className="reservation__input_container">
                     <label className="reservation__label" htmlFor="tablePreferences">Table Preferences</label>
-                    <textarea className="reservation__input" id="tablePreferences" name="tablePreferences" rows="4" ref={tablePreferencesRef} placeholder="By the window, close the bathroom, ect."></textarea>
+                    <textarea className="reservation__input" id="tablePreferences" rows="4" {...getFieldProps("tablePreferences")} placeholder="By the window, close the bathroom, ect."></textarea>
                 </div>
                 <div className="reservation__input_container">
                     <label className="reservation__label" htmlFor="name">Your name</label>
-                    <input className="reservation__input" id="name" type="text" name="name" required ref={nameRef} />
+                    <input className="reservation__input" id="name" type="text" {...getFieldProps("name")} />
+                    {errors.name && touched.name && <div className="reservation__errors">{errors.name}</div>}
                 </div>
                 <div className="reservation__input_container">
                     <label className="reservation__label" htmlFor="emailAddress">Your email address</label>
-                    <input className="reservation__input" id="emailAddress" type="email" name="emailAddress" ref={emailAddressRef} />
+                    <input className="reservation__input" id="emailAddress" type="email" {...getFieldProps("emailAddress")} />
+                    {errors.emailAddress && touched.emailAddress && <div className="reservation__errors">{errors.emailAddress}</div>}
                 </div>
                 <button className="reservation__submit button" role="submit" type="submit">Make Your Reservation</button>
             </form>
